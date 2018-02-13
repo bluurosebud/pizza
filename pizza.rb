4b8bb987e52cd58b709a3963b72c21dccba70eda
bluurosebud <bluurosebud@gmail.com>
2018-02-13 15:46:28 -0500

    made all toppings functions and first pizza function

--- pizza.rb
+++ pizza.rb
@@ -1,6 +1,53 @@
 def crust()
-	result=["stuffed","thin","pan","deep dish", "cheesy"].sample
-	p result
+	result=["stuffed","thin","pan","deep dish","cheesy"].sample
 end
+def sauce()
+	result=["house sauce","classic","extra thick","lite sauce"].sample
+end
+def cheese()
+	result=["cheddar","mozzarella","feta","provalone"].sample
+end
+def meat_topping()
+	result=["steak","pepperoni","chicken","bacon","ham"].sample
+end
+def veggi_topping()
+	result=["peppers","onions","olives","mushrooms","tomatoes","spinach"].sample
+end
+def specials()
+	result=["banana peppers","pineapple","cheese sause","garlic sauce"].sample
+end
+def pizzax()
+	p crust
+	p sauce
+	p cheese
+	p meat_topping
+	p veggi_topping
+	p specials
+end
+
+pizzax
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 crust
